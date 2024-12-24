bdd30a829096b0ff1c70b69c668358a4c6d8d3cf
hazemanwer2000 <hazemanwer2000@proton.me>
2024-12-24 15:52:09 +0200

    NEW: cookie-cutter!

---
+++ gui/cookie-cutter/run.py
@@ -0,0 +1,10 @@
+
+import automatey.GUI.GElements as GElements
+import automatey.GUI.GConcurrency as GConcurrency
+import automatey.GUI.GUtils as GUtils
+
+application = GElements.Application()
+
+
+
+application.run()
