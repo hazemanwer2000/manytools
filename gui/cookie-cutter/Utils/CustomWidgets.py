17e4a516dd7d62c23648b2b8b50499c006d38342
hazemanwer2000 <hazemanwer2000@proton.me>
2024-12-25 21:01:02 +0200

    Cookie-Cutter!

--- gui/cookie-cutter/Utils/CustomWidgets.py
+++ gui/cookie-cutter/Utils/CustomWidgets.py
@@ -5,6 +5,8 @@ import automatey.GUI.GUtils as GUtils
 import automatey.Abstract.Graphics as AbstractGraphics
 import automatey.Base.ColorUtils as ColorUtils
 
+import collections
+
 class FilterOption(GElements.Widgets.Complex.FilterList.FilterOption):
 
     def getData(self):
@@ -26,26 +28,36 @@ class FilterOptionFull(FilterOption):
     For option(s) with one or more cfg. param.
     '''
 
-    def __init__(self, cfgNames:list, cfgPlaceholders:list):
+    def __init__(self, cfgNames:list, cfgWidgets:list):
         
         # ? Setup option's title and container.
         self.rootTitleLabel = GElements.Widgets.Basics.Label(self.getName()) 
         self.rootVerticalContainer = GElements.Widgets.Containers.VerticalContainer(elementMargin=AbstractGraphics.SymmetricMargin(0), elementSpacing=5)
         self.rootVerticalContainer.getLayout().insertWidget(self.rootTitleLabel)
         
-        # ? For each cfg. param, setup a label, and a line-edit, then add it to (root-)container.
+        # ? For each cfg. param, setup a label (...), then add it to (root-)container.
         self.cfgLabels = []
-        self.cfgLineEdits = []
-        for cfgName, cfgPlaceholder in zip(cfgNames, cfgPlaceholders):
+        for cfgName, cfgWidget in zip(cfgNames, cfgWidgets):
             cfgLabel = GElements.Widgets.Basics.Label(cfgName)
-            cfgLineEdit = GElements.Widgets.Basics.LineEdit(placeholder=cfgPlaceholder)
             cfgVerticalContainer = GElements.Widgets.Containers.VerticalContainer(elementMargin=AbstractGraphics.SymmetricMargin(0), elementSpacing=5)
             cfgVerticalContainer.getLayout().insertWidget(cfgLabel)
-            cfgVerticalContainer.getLayout().insertWidget(cfgLineEdit)
+            cfgVerticalContainer.getLayout().insertWidget(cfgWidget)
             self.rootVerticalContainer.getLayout().insertWidget(GElements.Widgets.Decorators.Outline(cfgVerticalContainer,
                                                                                                      elementMargin=AbstractGraphics.SymmetricMargin(5)))
         
         super().__init__(self.rootVerticalContainer)
+
+class FilterOptionLineEditOnly(FilterOptionFull):
+    '''
+    For option(s) with line-input only.
+    '''
+    
+    def __init__(self, cfgNames:list, cfgPlaceholders:list):
+        self.cfgLineEdits = []
+        for cfgPlaceholder in cfgPlaceholders:
+            cfgLineEdit = GElements.Widgets.Basics.LineEdit(placeholder=cfgPlaceholder)
+            self.cfgLineEdits.append(cfgLineEdit)
+        super().__init__(cfgNames, self.cfgLineEdits)
 
 class FilterOptions:
 
@@ -55,7 +67,7 @@ class FilterOptions:
     class Grayscale(FilterOptionLess):
         pass
 
-    class BrightnessContrast(FilterOptionFull):
+    class BrightnessContrast(FilterOptionLineEditOnly):
         
         def __init__(self):
             self.cfgNames = [
@@ -68,7 +80,7 @@ class FilterOptions:
             ]
             super().__init__(self.cfgNames, self.cfgPlaceholders)
 
-    class GaussianBlur(FilterOptionFull):
+    class GaussianBlur(FilterOptionLineEditOnly):
         
         def __init__(self):
             self.cfgNames = [
@@ -79,7 +91,7 @@ class FilterOptions:
             ]
             super().__init__(self.cfgNames, self.cfgPlaceholders)
 
-    class Sharpen(FilterOptionFull):
+    class Sharpen(FilterOptionLineEditOnly):
         
         def __init__(self):
             self.cfgNames = [
@@ -92,7 +104,7 @@ class FilterOptions:
             ]
             super().__init__(self.cfgNames, self.cfgPlaceholders)
 
-    class Pixelate(FilterOptionFull):
+    class Pixelate(FilterOptionLineEditOnly):
 
         def __init__(self):
             self.cfgNames = [
@@ -106,17 +118,14 @@ class FilterOptions:
     class AddBorder(FilterOptionFull):
 
         def __init__(self):
-            self.cfgNames = [
-                'Thickness',
-                'Color',
-            ]
-            self.cfgPlaceholders = [
-                'e.g., 50',
-                'e.g., #ffffff',
-            ]
-            super().__init__(self.cfgNames, self.cfgPlaceholders)
+            
+            self.cfgName2Widget = collections.OrderedDict()
+            self.cfgName2Widget['Thickness'] = GElements.Widgets.Basics.LineEdit(placeholder='e.g., 50')
+            self.cfgName2Widget['Color'] = GElements.Widgets.Complex.ColorSelector(ColorUtils.Colors.BLACK)
+            
+            super().__init__(self.cfgName2Widget.keys(), self.cfgName2Widget.values())
 
-    class Crop(FilterOptionFull):
+    class Crop(FilterOptionLineEditOnly):
 
         def __init__(self):
             self.cfgNames = [
@@ -129,7 +138,7 @@ class FilterOptions:
             ]
             super().__init__(self.cfgNames, self.cfgPlaceholders)
 
-    class Resize(FilterOptionFull):
+    class Resize(FilterOptionLineEditOnly):
 
         def __init__(self):
             self.cfgNames = [
@@ -142,7 +151,7 @@ class FilterOptions:
             ]
             super().__init__(self.cfgNames, self.cfgPlaceholders)
 
-    class VideoFade(FilterOptionFull):
+    class VideoFade(FilterOptionLineEditOnly):
 
         def __init__(self):
             self.cfgNames = [
@@ -155,7 +164,7 @@ class FilterOptions:
             ]
             super().__init__(self.cfgNames, self.cfgPlaceholders)
 
-    class AudioFade(FilterOptionFull):
+    class AudioFade(FilterOptionLineEditOnly):
 
         def __init__(self):
             self.cfgNames = [
@@ -171,7 +180,7 @@ class FilterOptions:
     class AudioMute(FilterOptionLess):
         pass
 
-    class GIF(FilterOptionFull):
+    class GIF(FilterOptionLineEditOnly):
 
         def __init__(self):
             self.cfgNames = [
