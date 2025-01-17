8777ba719ef09f609d5ecdff5862d1d796ed7c5f
hazemanwer2000 <hazemanwer2000@proton.me>
2025-01-17 15:04:10 +0200

    Video Quickies: Clean-up!

--- cli/video_quickies.py
+++ cli/video_quickies.py
@@ -7,11 +7,13 @@ import time
 
 import automatey.OS.FileUtils as FileUtils
 import automatey.OS.ProcessUtils as ProcessUtils
+import automatey.Abstract.Graphics as Graphics
+import automatey.Base.ColorUtils as ColorUtils
 import automatey.Media.VideoUtils as VideoUtils
 import automatey.Media.ImageUtils as ImageUtils
 import automatey.Utils.RandomUtils as RandomUtils
-import automatey.Base.TimeUtils as TimeUtils
 import automatey.Formats.JSON as JSON
+import automatey.Utils.CLI as CLI
 
 # ? Get app's root directory.
 f_this = FileUtils.File(__file__)
@@ -22,6 +24,15 @@ f_constants = f_appDir.traverseDirectory('constants.json')
 constants = JSON.fromFile(f_constants)
 
 class Utils:
+    
+    Constants = {
+        'cli' : {
+            'text-color' : Graphics.TextColor(
+                foreground=ColorUtils.Colors.PURPLE,
+                background=ColorUtils.Colors.WHITE,
+            )
+        }
+    }
     
     @staticmethod
     def executeCommand(*args) -> int:
@@ -36,88 +47,6 @@ class Utils:
         f_tmpDir = FileUtils.File.Utils.getTemporaryDirectory().traverseDirectory(RandomUtils.Generation.String(7))
         f_tmpDir.makeDirectory()
         return f_tmpDir
-
-    class VocalTimer:
-        '''
-        A vocal timer, for CLI application(s) with long I/O operation(s).
-        '''
-        
-        INTERNAL_Constants = {
-            # (!) Note: In Seconds.
-            'sleep-time' : {
-                'upper' : 1.2,
-                'lower' : 0.8,
-            },
-        }
-        
-        def __init__(self):
-            
-            # ? Initialize large object(s).
-            self.queue = queue.Queue()
-            self.thread = threading.Thread(target=self.INTERNAL_runner)
-            
-            # ? Start the thread.
-            self.thread.start()
-        
-        def INTERNAL_runner(self):
-            
-            # ? Initialize working variable(s).
-            isHalted = True
-            referenceTime = None
-            message = None
-            messageToPrint = None
-            
-            while (True):
-                  
-                # ? Check if there's anything pending in the queue.
-                if not self.queue.empty():
-                    item = self.queue.get()
-                    if item['command'] == 'kill':
-                        break
-                    elif item['command'] == 'stop':
-                        isHalted = True
-                        print('')
-                    elif item['command'] == 'start':
-                        isHalted = False
-                        referenceTime = item['args']['reference-time']
-                        message = item['args']['message']
-                        messageToPrint = None
-                
-                # ? Report, if not halted.
-                if not isHalted:
-                    
-                    if messageToPrint != None:
-                        print('\b' * (len(messageToPrint)), end='', flush=True)
-                    time.sleep(
-                        RandomUtils.Generation.Float(
-                            Utils.VocalTimer.INTERNAL_Constants['sleep-time']['lower'],
-                            Utils.VocalTimer.INTERNAL_Constants['sleep-time']['upper'],
-                        )
-                    )
-                    deltaTime = TimeUtils.Time.getEpochTime() - referenceTime
-                    messageToPrint = ((message + ': ') if (message != None) else '') + str(deltaTime)
-                    print(messageToPrint, end='', flush=True)
-        
-        def start(self, message=None):
-            self.queue.put(item={
-                'command' : 'start',
-                'args' : {
-                    'reference-time' : TimeUtils.Time.getEpochTime(),
-                    'message' : message,
-                },
-            })
-        
-        def stop(self):
-            self.queue.put(item={
-                'command' : 'stop',
-                'args' : None,
-            })
-        
-        def kill(self):
-            self.queue.put(item={
-                'command' : 'kill',
-                'args' : None,
-            })
 
 class FFMPEG:
     
@@ -161,15 +90,15 @@ class CommandHandler:
                 )
             )
             vid = VideoUtils.Video(f_input)
-            vocalTimer = Utils.VocalTimer()
+            vocalTimer = CLI.VocalTimer()
             
             # ? Generate thumbnail(s).
             thumbnailCount = rows * cols
             f_tmpDir = Utils.getTemporaryDirectory()
             f_thumbnailDir = f_tmpDir.traverseDirectory('thumbnails')
-            vocalTimer.start(message='Elapsed Time')
+            vocalTimer.issueCommand(CLI.VocalTimer.Commands.StartTimer(label='Elapsed Time:', textColor=Utils.Constants['cli']['text-color']))
             vid.generateThumbnails(f_thumbnailDir, thumbnailCount)
-            vocalTimer.stop()
+            vocalTimer.issueCommand(CLI.VocalTimer.Commands.StopTimer())
             
             # ? Generate (joint) thumbnail.
             img_jointThumb = ImageUtils.Image.createByTiling(f_thumbnailDir.listDirectory(), rows, cols)
@@ -178,7 +107,7 @@ class CommandHandler:
             
             # ? Clean-up (...)
             FileUtils.File.Utils.recycle(f_tmpDir)
-            vocalTimer.kill()
+            vocalTimer.issueCommand(CLI.VocalTimer.Commands.DestroyTimer())
             
 @click.group()
 def cli():
