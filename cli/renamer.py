f27df6f12172fd34858d3fa796ecafdc2f5c3ca4
hazemanwer2000 <hazemanwer2000@proton.me>
2025-03-02 05:11:09 +0200

    CLI: Renamer!

--- cli/renamer.py
+++ cli/renamer.py
@@ -1,20 +1,27 @@
 
 import sys
 import traceback
+from pprint import pprint
 
 import automatey.OS.FileUtils as FileUtils
 import automatey.Base.ExceptionUtils as ExceptionUtils
+import automatey.Utils.Validation as Validation
 import automatey.OS.Specific.Windows as Windows
+
+fileConditional = {
+    True : (lambda x: x.isFile()),
+    False : (lambda x: True),
+}
 
 try:
 
-    f_srcDir = FileUtils.File(sys.argv[1])
-    f_fileList = f_srcDir.listDirectory()
-    
-    Windows.Utils.sort(f_fileList, lambda x: str(x))
-    
     prefix = input('Prefix [Optional]: ').strip()
     suffix = input('Suffix [Optional]: ').strip()
+    isFilesOnly = Validation.asBool(input('Process files only? (yes/no): ').strip())
+
+    f_srcDir = FileUtils.File(sys.argv[1])
+    f_fileList = f_srcDir.listDirectory(conditional=fileConditional[isFilesOnly])
+    Windows.Utils.sort(f_fileList, lambda x: str(x))
 
     code = input("Do you really want to? [Say 'yesido']: ").strip()
     if code != 'yesido':
@@ -22,7 +29,7 @@ try:
 
     digitCount = len(str(len(f_fileList)))
     for idx, f in enumerate(f_fileList):
-        newName = prefix + str(idx).zfill(digitCount) + suffix
+        newName = prefix + str(idx + 1).zfill(digitCount) + suffix
         FileUtils.File.Utils.rename(f, newName)
 
 except Exception as e:
