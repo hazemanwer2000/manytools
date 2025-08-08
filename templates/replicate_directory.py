6684a463ccd6eb61ddcd709adf5abe1576703e44
hazemanwer2000 <hazemanwer2000@proton.me>
2025-08-08 16:39:55 +0300

    Templates!

--- templates/replicate_directory.py
+++ templates/replicate_directory.py
@@ -13,14 +13,12 @@ def processFile(f_input:FileUtils.File, f_output:FileUtils.File):
 
 @click.command()
 @click.option('--input', required=True, help='Input directory.')
-@click.option('--output', required=True, help='Output path, where the output (i.e., replicated) directory will reside.')
-def cli(input:str, output:str):
+def cli(input:str):
     '''
     #REPLACE: Replicates a directory, by processing each file in the input directory, and optionally, generating a corresponding file in the output directory.
     '''
     f_inputDir = FileUtils.File(input)
-    f_baseOutputDir = FileUtils.File(output, f_inputDir.getName())
-    f_outputDir = FileUtils.File(FileUtils.File.Utils.Path.iterateName(str(f_baseOutputDir))) 
+    f_outputDir = FileUtils.File(FileUtils.File.Utils.Path.iterateName(str(f_inputDir)))
     FileUtils.File.Utils.mapDirectoryFiles(f_inputDir, f_outputDir, processFile)
 
 if __name__ == '__main__':
