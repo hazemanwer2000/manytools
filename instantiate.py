b8e3a13f1bf9d4cc8c9588030f9d8a420b47ede9
hazemanwer2000 <hazemanwer2000@proton.me>
2025-12-19 14:14:47 +0200

    Use jinja2!

--- instantiate.py
+++ instantiate.py
@@ -3,43 +3,43 @@ import automatey.OS.ProcessUtils as ProcessUtils
 import automatey.OS.FileUtils as FileUtils
 import automatey.Formats.JSON as JSON
 
-# ! Shared data.
+import jinja2
 
 f_cwd = FileUtils.File.Utils.getWorkingDirectory()
 
 # ? Load settings.
-
 f_settings = f_cwd.traverseDirectory('settings.json')
 settings = JSON.fromFile(f_settings)
 
 # ? Collect template file(s).
-
 f_templates = f_cwd.listDirectory(isRecursive=True, conditional=(lambda f: f.getExtension() == 'ttt'))
 
 # ? Process template file(s).
 
 for f_template in f_templates:
+
+    templateDict = dict()
     
-    # ? Setup formatter.
-    fileFormatter = ProcessUtils.FileTemplate.fromFile(f_template).createFormatter()
+    # ? Read template.
+    template = jinja2.Template(f_template.quickRead('t'))
     
     # ? Format (...)
     
     # ? ? Parameter: CURRENT-DIRECTORY
     f_templateDir = f_template.traverseDirectory('..')
     f_templateDirAbs = f_cwd.traverseDirectory(str(f_templateDir))
-    fileFormatter.assertParameter('current-directory', str(f_templateDirAbs))
+    templateDict['current_directory'] = str(f_templateDirAbs)
 
     # ? ? Parameter: BASE-DIRECTORY
-    fileFormatter.assertParameter('base-directory', str(f_cwd))
-    
+    templateDict['base_directory'] = str(f_cwd)
+
     # ? ? Replace 'global-macros'.
     global_macros = settings['global-macros']
     for macro_name in global_macros:
-        fileFormatter.assertParameter(macro_name.lower(), global_macros[macro_name])
+        templateDict[macro_name] = global_macros[macro_name]
     
-    # ? Write file instance.
+    # ? Instantiate template, and save.
     f_instance = FileUtils.File(FileUtils.File.Utils.Path.modifyName(str(f_template), extension=''))
     if f_instance.isExists():
         FileUtils.File.Utils.recycle(f_instance)
-    fileFormatter.saveAs(f_instance)
+    f_instance.quickWrite(template.render(**templateDict), 't')
